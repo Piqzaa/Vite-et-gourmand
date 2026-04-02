34817136cbbd1824deac416b42b623572cfa3ac4
Alex B <alex.berrel@gmail.com>
2026-04-03 01:23:35 +0200

    feat: ajout vérification + confirm mdp côté navigateur ,Lors de la modification dans le Profil

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,12 @@ import { initBurger } from "./modules/burger.js";
 import { initGallery } from "./modules/gallery.js";
 import { initStepper } from "./modules/stepper.js";
 import { initMenuFilters } from "./modules/filter.js";
+import { initProfil } from "./modules/profil.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initBurger();
   initGallery();
   initStepper();
+  initProfil();
   initMenuFilters();
 });
