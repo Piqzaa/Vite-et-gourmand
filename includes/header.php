404580da2060c3adbbad1339caf1347ea97a3081
Alex B <alex.berrel@gmail.com>
2026-04-03 19:04:32 +0200

    refactor(core): déplace functions.php dans php/include et met à jour les includes

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <?php 
-require_once __DIR__ . '/functions.php';
+require_once __DIR__ . '/../assets/php/includes/functions.php';
 require_once __DIR__ . '/../assets/php/includes/session.php';
 sessionStart();
 ?>
